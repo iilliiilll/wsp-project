4f14b5e7dce38bd32abc5e39d6044878b575c4c2
soowan <tndhks0708@naver.com>
2024-12-21 05:37:42 +0900

    mongo DB 연결 성공

---
+++ backend/src/main/java/swan20010708/Term_Project/TermProjectApplication.java
@@ -0,0 +1,13 @@
+package swan20010708.Term_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/swan20010708/Term_Project/TermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package swan20010708.Term_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
